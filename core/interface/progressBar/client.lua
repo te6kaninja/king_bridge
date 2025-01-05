2288768f06e73bee5e6565e9b291ab86859bcdca
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-01-05 15:11:21 +0200

    feat(progressBar): implement bridge functions for progress bar management

---
+++ core/interface/progressBar/client.lua
@@ -0,0 +1,28 @@
+local resource <const> = GetConvar('kbridge:progressBar', 'ox')
+
+function bridge.progressBar(data)
+    if resource == 'ox' then
+        lib.progressBar(data)
+    elseif resource == 'oxCircle' then
+        lib.progressBarCircle(data)
+    elseif resource == 'ls' then
+        exports.ls_progressbar:progressBar(data)
+    end
+end
+
+function bridge.cancelProgress()
+    if resource == 'ox' or resource == 'oxCircle' then
+        lib.cancelProgress()
+    elseif resource == 'ls' then
+        exports.ls_progressbar:cancel()
+    end
+end
+
+---@return boolean?
+function bridge.progressActive()
+    if resource == 'ox' or resource == 'oxCircle' then
+        return lib.progressActive()
+    elseif resource == 'ls' then
+        return exports.ls_progressbar:isProgressActive()
+    end
+end
