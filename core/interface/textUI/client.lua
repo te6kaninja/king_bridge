1f6ea450dad3d1a53db7c45d01f2ae221df93c8d
Georgi Stoyanov <stoyanow2707@gmail.com>
2025-01-05 12:47:56 +0200

    feat(textUI): add client interface for managing text UIs

---
+++ core/interface/textUI/client.lua
@@ -0,0 +1,30 @@
+local resource <const> = GetConvar('kbridge:textUI', 'ox')
+
+---@param text string
+---@param data table?
+function kbridge.showTextUI(text, data)
+    if text then
+        if resource == 'ox' then
+            lib.showTextUI(text, data)
+        elseif resource == 'ls' and data then
+            exports.ls_textui:showTextUI(data.id, data.key, text)
+        end
+    end
+end
+
+---@param id any
+function kbridge.hideTextUI(id)
+    if resource == 'ox' then
+        lib.hideTextUI()
+    elseif resource == 'ls' and id then
+        exports.ls_textui:hideTextUI(id)
+    end
+end
+
+-- Works only with text UIs that support it themselves
+---@return any[] | table?
+function kbridge.getTextUIs()
+    if resource == 'ls' then
+        return exports.ls_textui:getTextUIs()
+    end
+end
